c9b75edf0a46f85db6c7e03d27ba538f8e70cc4f
Norbert Forero <forero-98@hotmail.com>
2022-01-19 00:17:49 -0500

    Agregar funcion task post

--- App Express/app.js
+++ App Express/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //Cargar routes
 const hello_routes = require("./routes/routeHello");
+const task_routes = require("./routes/task");
 
 //Rutas Base
 app.use("/api", hello_routes);
+app.use("/api", task_routes);
 
 module.exports = app;
