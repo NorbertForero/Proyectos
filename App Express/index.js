635e54b9cb42139f370296f6ad94c53b126e89b2
Norbert Forero <forero-98@hotmail.com>
2022-01-23 22:26:58 -0500

    Deploy

--- App Express/index.js
+++ App Express/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3977;
 const urlMongoDB =
   "mongodb+srv://nf:Stevengalan@clusternf.ih7gw.mongodb.net/dbmongo";
 
